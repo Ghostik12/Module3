02ea9199eb101811db2b679bd17fb09b5058cf1b
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-09-21 23:07:10 +0300

    Unit 4.1.17

---
+++ Module4/Module4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module4 { 
+
+    public class Module4
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write your favorite color");
+
+            var color = Console.ReadLine();
+            
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color red");
+            }
+            else if (color == "green") 
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color green");
+            }
+            else if (color == "blue") 
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color blue");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (color == "yellow")
+            {
+                Console.BackgroundColor= ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color yellow");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color cyan");
+            }
+        }
+    }
+
+
+}
