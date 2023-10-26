3dfba12da06bf579b3bf82a9d7ebfb6b50fd8222
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-10-26 19:27:16 +0300

    DriveAndDirectory

---
+++ Module8/Module8/Drive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8
+{
+    class Drive
+    {
+        public string Name { get; }
+        public long Capacity { get; }
+        public long FreeSpace { get; }
+
+        public Drive(string name, long capacity, long freeSpace)
+        {
+            Name = name;
+            Capacity = capacity;
+            FreeSpace = freeSpace;
+        }
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+    }
+}
---
+++ Module8/Module8/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //GetCatalogs();
+            //try
+            //{
+            //    DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\" /* Или С:\\ для Windows */ );
+            //    if (dirInfo.Exists)
+            //    {
+            //        Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+            //    }
+
+            //    DirectoryInfo difnew = new DirectoryInfo (@"C:/newDirectory");
+            //    if (!difnew.Exists)
+            //    {
+            //        difnew.Create();
+            //    }
+
+            //    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+
+            //    difnew.Delete(true);
+
+            //    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //DirectoryInfo dir1 = new DirectoryInfo(@"C:/newDirectory");
+            //string dirs = "C:/newDirectoryNew";
+
+            //if (dir1.Exists && !Directory.Exists(dirs))
+            //{
+            //    dir1.MoveTo(dirs);
+            //}
+
+            //try
+            //{
+            //    DirectoryInfo dirInfo = new DirectoryInfo(@"C:/User/Ghostman/Desktop/testFolder");
+            //    string dirStr = "C:/User/Ghostman/Desktop/Basket";
+            //    if (!dirInfo.Exists)
+            //    {
+            //        dirInfo.Create();
+            //    }
+            //    if(dirInfo.Exists && !Directory.Exists(dirStr))
+            //    {
+            //        dirInfo.MoveTo(dirStr);
+            //    }
+            //}
+            //catch (Exception e) 
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+        }
+
+        //public static void GetCatalogs()
+        //{
+        //    string dirName = @"C:\\";
+
+        //    if (Directory.Exists(dirName))
+        //    {
+        //        Console.WriteLine("Papki");
+        //        string[] dirs = Directory.GetDirectories(dirName);
+
+        //        foreach (string dir in dirs)
+        //        {
+        //            Console.WriteLine(dir);
+        //        }
+
+        //        Console.WriteLine();
+        //        Console.WriteLine("Files");
+        //        string[] fils = Directory.GetFiles(dirName);
+
+        //        foreach(string fil in fils)
+        //        {
+        //            Console.WriteLine(fil);
+        //        }
+        //    }
+        //}
+    }
+}
