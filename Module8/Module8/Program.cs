f9387fd9bfb65de434d17f546d43f6fdbe6cfa5b
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-10-26 20:03:11 +0300

    CreateCopyDeleteFile

--- Module8/Module8/Program.cs
+++ Module8/Module8/Program.cs
@@ -10,17 +10,42 @@ namespace Module8
     {
         public static void Main(string[] args)
         {
-            string FilePath = @"D:/Проекты/Module3/Module8/Module8/Program.cs";
-            if (File.Exists(FilePath))
+            string tempFile = Path.GetTempFileName();//используем генерацию имени файла.
+            var fileInfo = new FileInfo(tempFile);//Создаем объект класса FileInfo.
+
+            using (StreamWriter sw = fileInfo.CreateText())
             {
-                using (StreamReader sr = new StreamReader(FilePath))
+                sw.WriteLine("Seva");
+                sw.WriteLine("Dima");
+                sw.WriteLine("Nastya");
+                sw.WriteLine(DateTime.Now);
+            }
+
+            using (StreamReader sr = fileInfo.OpenText())
+            {
+                string line = "";
+                while((line = sr.ReadLine()) != null)
                 {
-                    string line = "";
-                    while((line = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(line);
-                    }
+                    Console.WriteLine(line);
                 }
+            }
+
+            try
+            {
+                string tempFile2 = Path.GetTempFileName();
+                var fileInfo2 = new FileInfo(tempFile2);
+
+                fileInfo2.Delete(); //Убедимся, что файл назначения точно отсутствует
+
+                fileInfo.CopyTo(tempFile2);//Копируем информацию
+                Console.WriteLine($"{fileInfo} Copy to file {tempFile2}");
+
+                fileInfo.Delete();//Удаляем ранее созданный файл.
+                Console.WriteLine($"File {fileInfo} delete");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message );
             }
         }
     }
