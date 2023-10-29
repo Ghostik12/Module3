7d33873c04b04f3d7788a6c055b42a43119efa53
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-10-29 16:06:43 +0300

    Task3

--- Module8/Module8/Program.cs
+++ Module8/Module8/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Module8
 {
@@ -14,15 +16,15 @@ namespace Module8
         {
             string path = @"C:\\Users\\Ghosman\\Desktop\\Новая папка\\";
             long dirSize = 0;
+            long dirSpace = 0;
+            long dirSizeNow = 0;
             dirSize = GetSize(path, ref dirSize);
-            if (dirSize == 0) 
-            {
-                Console.WriteLine($"Каталог {path} пуст");
-            }
-            else
-            {
-                Console.WriteLine($"Размер каталога {path} составляет {dirSize} Байт");
-            }
+            Console.WriteLine($"Исходный размер папки: {dirSize} байт");
+            DeleteFileAndDirectory(path);
+            dirSpace = dirSize - GetSize(path, ref dirSpace);
+            Console.WriteLine($"Освобожденно: {dirSpace}  байт");
+            dirSizeNow = GetSize(path, ref dirSizeNow);
+            Console.WriteLine($"Текущий размер папки: {dirSizeNow} байт");
         }
 
         public static long GetSize(string path, ref long size)
@@ -49,6 +51,34 @@ namespace Module8
                 Console.WriteLine(ex.Message);
             }
             return size;
+        }
+        public static void DeleteFileAndDirectory(string path)
+        {
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            FileInfo[] fileInfo = dir.GetFiles();
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    foreach (var file1 in fileInfo)
+                    {
+                        if ((DateTime.Now - file1.LastWriteTime) > TimeSpan.FromMinutes(2))
+                            file1.Delete();
+                    }
+                    foreach (var file in dirs)
+                    {
+                        DeleteFileAndDirectory(file.FullName);
+                        if ((DateTime.Now - file.LastWriteTime) > TimeSpan.FromMinutes(2))
+                            file.Delete(true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
