92de3193a381ec1876cf496371649dc6481c96e7
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-11-11 22:56:06 +0300

    Module9(1)

---
+++ Module9/Module9/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Module9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception ex = new Exception("Произошло исключение");
+            ex.Data.Add("Дата создания исключения", DateTime.Now);
+            ex.HelpLink = "https://yandex.ru";
+
+            try
+            {
+                throw new RankException("Fail");
+            }
+            catch (Exception exception)
+            { 
+                Console.WriteLine(exception.Message);
+                Console.WriteLine(exception.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("Сработал finally");
+            }
+            Sum sum = Minus;
+            sum += Plus;
+
+            sum -= Plus;
+            sum(9, 4);
+            //Strin str = Str;
+            //str.Invoke();
+            //SumFiguer SM = Figuer;
+            //int sm = SM(10, 15);
+            //Console.WriteLine(sm);
+            //CheckLenght cL = Check;
+            //bool chec = cL("Check");
+            //Console.WriteLine(chec);
+
+            Action<string> action = Str;
+            action("srt");
+            Func<int, int, int> func = Figuer; 
+            int result = func(10, 15);
+            Console.WriteLine(result);
+            Predicate<string> predicate = Check;
+            bool chec = Check("Mamba");
+            Console.WriteLine(chec);
+
+            ShowMassege sh = (string massege) => Console.WriteLine($"{massege}");
+            sh.Invoke("Hello world");
+
+            RandomNumber rn = () => new Random().Next(0, 100);
+            int random = rn.Invoke();
+            Console.WriteLine(random);
+        }
+
+        delegate int RandomNumber();
+        delegate void ShowMassege(string massege);
+        public delegate void Sum(int a, int b);
+        //public delegate void Strin();
+        //public delegate int SumFiguer(int a, int b);
+        //public delegate bool CheckLenght(string s);
+
+        static void Minus (int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Plus(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Str(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        private static int Figuer(int a, int b) 
+        {
+            return a + b;
+        }
+
+        static bool Check(string check)
+        {
+            if (check.Length < 3)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
