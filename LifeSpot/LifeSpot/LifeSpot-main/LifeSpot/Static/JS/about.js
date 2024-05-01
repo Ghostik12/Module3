a6616f00bf696a4bfe94dab7a7b896c83b6ed412
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2024-05-01 22:52:29 +0300

    Task 29.7

---
+++ LifeSpot/LifeSpot/LifeSpot-main/LifeSpot/Static/JS/about.js
@@ -0,0 +1,24 @@
+﻿function getReview() {
+    let review = {}
+
+    review["username"] = promt('Как вас зовут?')
+    if (review["username"] == null) {
+        return
+    }
+
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["commemt"] == null) {
+        return
+    }
+
+    review["date"] = new Date().toLocaleString()
+
+    writeReview(review)
+}
+
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
--- LifeSpot/LifeSpot/LifeSpot-main/LifeSpot/Static/JS/index.js
+++ LifeSpot/LifeSpot/LifeSpot-main/LifeSpot/Static/JS/index.js
@@ -1,13 +1,60 @@
-﻿function filter() {
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+﻿function filter(inputParseFuncction) {
     let elements = document.getElementsByClassName('video-container');
 
     for (let i = 0; i <= elements.length; i++) {
         let videoText = elements[i].querySelector('h3').innerText;
-        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+        if (!videoText.toLowerCase().includes(inputParseFuncction.toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
         }
     }
+}
+// создадим объект Map для хранения сессии
+let session = new Map();
+
+function handleSession() {
+    // создадим объект Map для хранения сессии
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+
+    // Проверка на возраст и сохранение сессии
+    //if (session.get("age") >= 18) {
+    //    let startDate = new Date().toLocaleString();
+
+    //    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+    //    session.set("startDate", startDate)
+    //}
+    //else {
+    //    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+    //    window.location.href = "http://www.google.com"
+    //}
+    //// Вывод в консоль
+    //for (let result of session) {
+    //    console.log(result)
+    //}
+
+    //return session;
+}
+
+function checkAge() {
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+let sessionLog = function logSession(session) {
+    for (let result of session)
+        console.log(result)
 }
